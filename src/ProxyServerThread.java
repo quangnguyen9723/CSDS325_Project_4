8282357b1e354bde4127901caa1a693ca79ecbe6
Quang.Ng.2003 <78969220+quangnguyen9723@users.noreply.github.com>
2023-04-20 11:40:25 -0400

    reformat proxyThread
    Signed-off-by: Quang.Ng.2003 <78969220+quangnguyen9723@users.noreply.github.com>

--- src/ProxyServerThread.java
+++ src/ProxyServerThread.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.Socket;
 import java.net.URL;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProxyServerThread extends Thread {
     /**
@@ -11,6 +12,8 @@ public class ProxyServerThread extends Thread {
      * 3. forward the response back to wget
      */
     private final Socket clientSocket;
+    private static final ConcurrentHashMap<String, byte[]> cache = new ConcurrentHashMap<>();
+
 
     public ProxyServerThread(Socket socket) {
         this.clientSocket = socket;
@@ -23,56 +26,65 @@ public class ProxyServerThread extends Thread {
             InputStream clientIn = clientSocket.getInputStream();
             BufferedReader clientReader = new BufferedReader(new InputStreamReader(clientIn));
             String requestLine = clientReader.readLine();
-            System.out.println(requestLine); //---------------------
 
             // Parse the request and get the URL
             String[] requestParts = requestLine.split(" ");
-            String method = requestParts[0];
             String url = requestParts[1];
 
-            // Send the request to the destination server using HttpURLConnection
-            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod(method);
+            // Check if the response is already cached
+            byte[] response = cache.get(url);
 
-            // Read headers from client and set them to the HttpURLConnection
-            String line;
-            while (!(line = clientReader.readLine()).isEmpty()) {
-                System.out.println(line); //---------------------
-                int index = line.indexOf(":");
-                if (index != -1) {
-                    connection.setRequestProperty(line.substring(0, index), line.substring(index + 1).trim());
+            if (response == null) {
+                // Send the request to the destination server using HttpURLConnection
+                HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+                connection.setRequestMethod(requestParts[0]);
+
+                // Read headers from client and set them to the HttpURLConnection
+                String line;
+                while (!(line = clientReader.readLine()).isEmpty()) {
+                    int index = line.indexOf(":");
+                    if (index != -1) {
+                        connection.setRequestProperty(line.substring(0, index), line.substring(index + 1).trim());
+                    }
                 }
+
+                // Get the server's response
+                InputStream serverIn = connection.getInputStream();
+                ByteArrayOutputStream serverResponse = new ByteArrayOutputStream();
+
+                // Write the status line and headers to serverResponse
+                String statusLine = "HTTP/1.1 " + connection.getResponseCode() + " " + connection.getResponseMessage() + "\r\n";
+                serverResponse.write(statusLine.getBytes());
+
+                for (String key : connection.getHeaderFields().keySet()) {
+                    if (key != null) {
+                        String headerLine = key + ": " + connection.getHeaderField(key) + "\r\n";
+                        serverResponse.write(headerLine.getBytes());
+                    }
+                }
+                serverResponse.write("\r\n".getBytes());
+
+                // Read the response body into serverResponse
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = serverIn.read(buffer)) != -1) {
+                    serverResponse.write(buffer, 0, bytesRead);
+                }
+
+                // Cache the response
+                response = serverResponse.toByteArray();
+                cache.put(url, response);
+
+                // Close HttpURLConnection
+                connection.disconnect();
             }
 
-            // Get the server's response
-            InputStream serverIn = connection.getInputStream();
-
-            // Write the status line and headers back to the client
+            // Send the cached response back to the client
             OutputStream clientOut = clientSocket.getOutputStream();
-            PrintWriter clientWriter = new PrintWriter(new OutputStreamWriter(clientOut));
-            clientWriter.println("HTTP/1.1 " + connection.getResponseCode() + " " + connection.getResponseMessage());
-            System.out.println();
-            System.out.println("HTTP/1.1 " + connection.getResponseCode() + " " + connection.getResponseMessage()); //---------------------
-            for (String key : connection.getHeaderFields().keySet()) {
-                if (key != null) {
-                    System.out.println(key + ": " + connection.getHeaderField(key)); //---------------------
-                    clientWriter.println(key + ": " + connection.getHeaderField(key));
-                }
-            }
-            clientWriter.println();
-            clientWriter.flush();
-
-            // Send the server's response back to the client
-            byte[] buffer = new byte[4096];
-            int bytesRead;
-            while ((bytesRead = serverIn.read(buffer)) != -1) {
-                System.out.println(new String(buffer)); //---------------------
-                clientOut.write(buffer, 0, bytesRead);
-            }
+            clientOut.write(response);
             clientOut.flush();
 
-            // Close all connections
-            connection.disconnect();
+            // Close the client connection
             clientSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
